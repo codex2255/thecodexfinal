506efda9f81efe0865e9f21a902bc2cc06b86aeb
JneezNZ <lansdown.nz@gmail.com>
2025-03-14 15:07:35 +1300

    14 mar end of day

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const correctPassword = "wakeUP";
 
-$(document).ready(function() {
+$(document).ready(function () {
     var currentPage = 0;
     var pages = $('.page');
 
-   
+
     function scrollToPage(pageIndex) {
         if (pageIndex >= 0 && pageIndex < pages.length) {
             $('html, body').animate({
@@ -14,27 +14,39 @@ $(document).ready(function() {
         }
     }
 
-   
-    $(window).on('wheel', function(e) {
+
+    $(window).on('wheel', function (e) {
         if (e.originalEvent.deltaY > 0) {
-           
+
             scrollToPage(currentPage + 1);
         } else {
-            
+
             scrollToPage(currentPage - 1);
         }
     });
 
-    
+
     scrollToPage(currentPage);
 });
 
 
 function showPage(pageId) {
     // Hide all pages
-    
-    
+
+
     // Show the selected page
     const selectedPage = document.getElementById(pageId);
     selectedPage.classList.add('active');
 }
+
+
+const sidebar = document.getElementById('sidebar');
+const sidebarToggle = document.getElementById('sidebarToggle');
+
+// Check if the elements are correctly selected
+console.log(sidebar, sidebarToggle);
+
+// Event listener to toggle the sidebar visibility
+sidebarToggle.addEventListener('click', function() {
+    sidebar.classList.toggle('show'); // Toggle the 'show' class to slide the sidebar in/out
+});
